f9045b96cc788006fc48c14e9bd982d94a0125d3
Simon Soriano <simon0191@gmail.com>
2017-05-28 07:42:09 -0500

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+apt-get install -y git docker-ce
+
+wget https://cl.ly/2R1k360u1Y3p/metaops-node
+chmod +x metaops-node
